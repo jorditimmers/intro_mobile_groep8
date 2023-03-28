fa0a4f59c5c1ace308ead26e6add99fe409bf9f8
Robbe Wellens <wellensrobbe@gmail.com>
2023-03-28 20:49:20 +0200

    Add 'Map' and 'Indicate Departure' button to Home page

--- easypark_app/lib/ui/home/homepage.dart
+++ easypark_app/lib/ui/home/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:easypark_app/strings.dart';
+import 'package:easypark_app/ui/map/map_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,12 +14,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return Scaffold(
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             _buildTitle(),
+            ElevatedButton(
+              style: style,
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => MapPage(),
+                  ),
+                );
+              },
+              child: const Text('Map'),
+            ), // Map Button
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: style,
+              onPressed: () {},
+              child: const Text('Indicate Departure'),
+            ), //Departure button
           ],
         ),
       ),
