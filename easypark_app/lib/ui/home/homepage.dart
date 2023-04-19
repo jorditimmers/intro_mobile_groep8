1f2a2e528ca88dfdb21da072de6fc8c54d6612fc
Robbe Wellens <wellensrobbe@gmail.com>
2023-04-19 17:13:28 +0200

    Add buttons to Home page

--- easypark_app/lib/ui/home/homepage.dart
+++ easypark_app/lib/ui/home/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:easypark_app/strings.dart';
 import 'package:easypark_app/ui/map/map_page.dart';
 import 'package:flutter/material.dart';
+import '../headerbar/headerbar.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
@@ -17,32 +18,49 @@ class _HomePageState extends State<HomePage> {
     final ButtonStyle style =
         ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            _buildTitle(),
-            ElevatedButton(
-              style: style,
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => MapPage(),
+        appBar: headerBar(),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero),
+                    ),
+                    child: Text('Map'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MapPage()),
+                      );
+                    },
                   ),
-                );
-              },
-              child: const Text('Map'),
-            ), // Map Button
-            const SizedBox(height: 30),
-            ElevatedButton(
-              style: style,
-              onPressed: () {},
-              child: const Text('Indicate Departure'),
-            ), //Departure button
-          ],
-        ),
-      ),
-    );
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero),
+                    ),
+                    child: Text('Indicate Departure'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MapPage()),
+                      );
+                    },
+                  ),
+                ),
+              ]),
+        ));
   }
 
   Widget _buildTitle() => Text(
