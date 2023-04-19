608f04c00eb3ac2d90da22711c8c1f16312b96ca
Robbe Wellens <wellensrobbe@gmail.com>
2023-04-19 15:46:22 +0200

    Add buttons to Home page

---
+++ easypark_app/lib/ui/home/homepage.dart
@@ -0,0 +1,70 @@
+import 'package:easypark_app/strings.dart';
+import 'package:easypark_app/ui/map/map_page.dart';
+import 'package:flutter/material.dart';
+import '../headerbar/headerbar.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key, required this.title}) : super(key: key);
+
+  final title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: headerBar(),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero),
+                    ),
+                    child: Text('Map'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MapPage()),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero),
+                    ),
+                    child: Text('Indicate Departure'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MapPage()),
+                      );
+                    },
+                  ),
+                ),
+              ]),
+        ));
+  }
+
+  Widget _buildTitle() => Text(
+        title,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      );
+}
