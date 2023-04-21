33c3ffc539b653459292bf22045fdffa5bd44626
Jordi Timmers <timmersjordi@outlook.com>
2023-04-21 14:26:06 +0200

    Adding User model

--- easypark_app/lib/main.dart
+++ easypark_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'app/easypark_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firstore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(
