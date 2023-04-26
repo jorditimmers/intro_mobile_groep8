4896e5c1a3edccc4b60d758e19dd25a692928307
Robbe Wellens <wellensrobbe@gmail.com>
2023-04-26 13:12:31 +0200

    Run app before Firebase initialisation

--- easypark_app/lib/main.dart
+++ easypark_app/lib/main.dart
@@ -4,9 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  runApp(EasyParkApp());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  runApp(EasyParkApp());
 }
