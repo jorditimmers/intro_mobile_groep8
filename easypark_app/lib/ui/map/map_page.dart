40bc88cd8819e962418c1bcffc91b7a20660b48b
Robbe Wellens <wellensrobbe@gmail.com>
2023-04-26 13:13:43 +0200

    Add spot reservation and departure options to map

--- easypark_app/lib/ui/map/map_page.dart
+++ easypark_app/lib/ui/map/map_page.dart
@@ -2,31 +2,154 @@ import 'package:easypark_app/ui/headerbar/headerbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  //List<Marker> markers = [];
   final MapController mapController = MapController.withPosition(
     initPosition: GeoPoint(
-      latitude: 51.23024625125789,
-      longitude: 4.416128099999962,
+      latitude: 51.2288,
+      longitude: 4.41562,
     ), //AP Hogeschool Campus Ellermanstraat
     areaLimit: BoundingBox(
-      east: 25,
-      north: 25,
-      south: 25,
-      west: 25,
+      east: 4.41721,
+      north: 51.22978,
+      south: 51.22786,
+      west: 4.41366,
     ),
   );
 
-  MapPage({super.key});
+  bool isReserved = false;
+
+  dynamic addMarkerOnTap() {
+    mapController.listenerMapSingleTapping.addListener(() {
+      if (mapController.listenerMapSingleTapping.value != null) {
+        GeoPoint p = mapController.listenerMapSingleTapping.value!;
+        mapController.addMarker(p);
+        showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.blue,
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5))),
+                              ),
+                              child: Text('Reserve'),
+                              onPressed: () {
+                                isReserved = true;
+                                Navigator.pop(context);
+                              }),
+                        ),
+                        SizedBox(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white60,
+                                backgroundColor: Colors.black45,
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5))),
+                              ),
+                              child: Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              }),
+                        )
+                      ],
+                    ),
+                  ));
+            }).then((value) => {
+              mapController.removeMarker(p),
+              if (isReserved)
+                {
+                  mapController.addMarker(p,
+                      markerIcon: const MarkerIcon(
+                        icon: Icon(
+                          Icons.person_pin_circle,
+                          color: Colors.lightGreen,
+                          size: 84,
+                        ),
+                      ))
+                },
+              isReserved = false
+            });
+      }
+    });
+  }
+
+  void indicateDeparture() {}
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
       appBar: headerBar(),
       body: OSMFlutter(
+        onMapIsReady: addMarkerOnTap(),
+        onGeoPointClicked: (p) {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.blue,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                          ),
+                          onPressed: () {
+                            mapController.removeMarker(p);
+                            Navigator.pop(context);
+                          },
+                          child: Text('Depart'),
+                        ),
+                      ),
+                      SizedBox(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.black45,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5))),
+                          ),
+                          onPressed: () {
+                            mapController.removeMarker(p);
+                            Navigator.pop(context);
+                          },
+                          child: Text('Cancel'),
+                        ),
+                      )
+                    ],
+                  )),
+                );
+              });
+        },
         controller: mapController,
         trackMyPosition: false,
-        initZoom: 16,
-        minZoomLevel: 13,
+        initZoom: 18,
+        minZoomLevel: 18,
         maxZoomLevel: 19,
         stepZoom: 1.0,
         userLocationMarker: UserLocationMaker(
@@ -56,6 +179,6 @@ class MapPage extends StatelessWidget {
           ),
         )),
       ),
-    ));
+    );
   }
 }
